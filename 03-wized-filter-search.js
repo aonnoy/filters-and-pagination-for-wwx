f7e05d974c23db245bcf6bbadb6ad7e1084ad82f
aonnoy <70891467+aonnoy@users.noreply.github.com>
2023-12-18 23:39:31 +0000

    Update 03-wized-filter-search.js

--- 03-wized-filter-search.js
+++ 03-wized-filter-search.js
@@ -1,1 +1,63 @@
-window.Wized=window.Wized||[];window.Wized.push((Wized)=>{let debounceTimer;const debounce=(e,t)=>{return(...n)=>{clearTimeout(debounceTimer);debounceTimer=setTimeout(()=>e.apply(this,n),t)}};const executeWizedRequest=async(e)=>{try{await Wized.requests.execute(e);console.log(`Request ${e} executed successfully`)}catch(t){console.error(`Error executing request ${e}:`,t)}};const handleSearchInputChange=debounce((e)=>{const t=e.getAttribute("wized-filter-search-variable");const n=document.querySelector("[wized-filter-current-page]").getAttribute("wized-filter-current-page");const r=document.querySelector("[wized-filter-search-request]").getAttribute("wized-filter-search-request");Wized.data.v[t]=e.value;if(n&&Wized.data.v.hasOwnProperty(n)){Wized.data.v[n]=1}r&&executeWizedRequest(r)},500);const attachListeners=()=>{document.querySelectorAll("input[wized-filter-search-variable]").forEach((e)=>{e.removeEventListener("input",handleSearchInputChange);e.addEventListener("input",()=>handleSearchInputChange(e))})};attachListeners()});
+window.Wized = window.Wized || [];
+window.Wized.push((Wized) => {
+  let debounceTimer;
+
+  // Debounce function
+  const debounce = (func, delay) => {
+    return (...args) => {
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(this, args), delay);
+    };
+  };
+
+  // Function to execute Wized request
+  const executeWizedRequest = async (requestName) => {
+    try {
+      await Wized.requests.execute(requestName);
+      console.log(`Request ${requestName} executed successfully`);
+    } catch (error) {
+      console.error(`Error executing request ${requestName}:`, error);
+    }
+  };
+
+  // Function to handle search input changes
+  const handleSearchInputChange = debounce((input) => {
+    const searchVariableName = input.getAttribute(
+      "wized-filter-search-variable"
+    );
+    const currentPageVariableName = document
+      .querySelector("[wized-filter-current-page]")
+      .getAttribute("wized-filter-current-page");
+    const requestName = document
+      .querySelector("[wized-filter-search-request]")
+      .getAttribute("wized-filter-search-request");
+
+    // Update Wized variable with search value
+    Wized.data.v[searchVariableName] = input.value;
+
+    // Reset current page to 1
+    if (
+      currentPageVariableName &&
+      Wized.data.v.hasOwnProperty(currentPageVariableName)
+    ) {
+      Wized.data.v[currentPageVariableName] = 1;
+    }
+
+    // Execute the Wized request
+    if (requestName) {
+      executeWizedRequest(requestName);
+    }
+  }, 500);
+
+  // Function to attach event listeners
+  const attachListeners = () => {
+    document
+      .querySelectorAll("input[wized-filter-search-variable]")
+      .forEach((input) => {
+        input.removeEventListener("input", handleSearchInputChange); // Remove existing listener
+        input.addEventListener("input", () => handleSearchInputChange(input)); // Reattach listener
+      });
+  };
+
+  attachListeners(); // Initialize event listeners
+});
