ca8996c7e9ec762bd1429793bc49655a961fc721
aonnoy <70891467+aonnoy@users.noreply.github.com>
2023-12-18 23:40:04 +0000

    Update 05-wized-filter-category-tag.js

--- 05-wized-filter-category-tag.js
+++ 05-wized-filter-category-tag.js
@@ -1,1 +1,228 @@
-window.Wized=window.Wized||[];window.Wized.push((Wized)=>{const t=(e,n)=>{const t=document.createElement("div");t.setAttribute("wized-filter-tag","tag-template");t.classList.add("filter_tag");const r=document.createElement("div");r.setAttribute("wized-filter-tag","tag-text");r.classList.add("filter_tag-text");r.textContent=`${e}: ${n}`;const i=document.createElement("div");i.setAttribute("wized-filter-tag","tag-remove");i.classList.add("filter_tag-remove");t.appendChild(r);t.appendChild(i);const o=document.querySelector('[wized-filter-tag="wrapper"]');o&&o.appendChild(t)};const r=(e)=>{const n=e.target;const r=n.closest("[wized-filter-tag-category]");if(r){const i=r.getAttribute("wized-filter-tag-category");"checkbox"===n.type?l(n,i):"radio"===n.type?c(n,i):"select"===n.tagName.toLowerCase()&&h(n,i)}};const i=(e,n)=>{const t=e.nextElementSibling;if(e.checked){t=e.nextElementSibling;t&&t.textContent.trim()&&t.textContent.trim().startsWith(`${n}: `)&&o(n,t.textContent.trim())}};const o=(e,n)=>{const t=document.querySelector('[wized-filter-tag="wrapper"]');const r=[...t.querySelectorAll('[wized-filter-tag="tag-template"]')].find((e)=>e.querySelector('[wized-filter-tag="tag-text"]').textContent===`${e}: ${n}`);r&&r.remove()};const c=(e,n)=>{d(n);if(e.checked){const t=e.nextElementSibling;t&&t.textContent.trim()&&t.textContent.trim().startsWith(`${n}: `)&&u(n,t.textContent.trim())}};const s=(e,n)=>{d(n);const t=e.options[e.selectedIndex];t&&t.value&&u(n,t.text.trim())};const u=(e,n)=>{const t=document.querySelector('[wized-filter-tag="wrapper"]');t&&t.classList.contains("filter_tags-wrapper")&&t.appendChild(t);const r=document.querySelectorAll(`[wized-filter-tag-category="${e}"]`);r.forEach((e)=>{"checkbox"===e.querySelector('input[type="checkbox"]').type?p(e,n):"radio"===e.querySelector('input[type="radio"]').type&&f(e,n),"select"===e.tagName.toLowerCase()&&(e.selectedIndex=0,e.dispatchEvent(new Event("change")))})};const p=(e,n)=>{const t=Array.from(e.querySelectorAll('input[type="checkbox"]')).find((e)=>e.nextElementSibling.textContent.trim()===n);t&&(t.checked=!1,t.dispatchEvent(new Event("change",{bubbles:!0,cancelable:!0})))};const f=(e,n)=>{g(n);if(e.querySelector('input[type="radio"]').checked){const t=Array.from(e.querySelectorAll('input[type="radio"]')).find((e)=>e.nextElementSibling.textContent.trim()===n);t&&(t.checked=!1,t.closest(`[wized-filter-tag-category="${n}"]`).querySelector(".w-radio-input").classList.contains("w--redirected-checked")&&t.closest(`[wized-filter-tag-category="${n}"]`).querySelector(".w-radio-input").classList.remove("w--redirected-checked"));const r=e.querySelector('input[type="radio"]').getAttribute("wized-variable");r&&(Wized.data.v[r]=null,window.executeWizedRequestDebounced&&window.executeWizedRequestDebounced())}};const h=(e,n)=>{g(n);e.selectedIndex=0,e.dispatchEvent(new Event("change"))};const d=(e)=>{const t=document.querySelector('[wized-filter-tag="wrapper"]');const r=[...t.querySelectorAll('[wized-filter-tag="tag-template"]')].filter((t)=>t.querySelector('[wized-filter-tag="tag-text"]').textContent.startsWith(`${e}:`));r.forEach((e)=>e.remove())};const l=(e,n)=>{const t=e.nextElementSibling;if(e.checked){const t=e.nextElementSibling;t&&t.textContent.trim()&&t.textContent.trim().startsWith(`${n}: `)&&o(n,t.textContent.trim())}};const g=(e)=>{const t=document.querySelector('[wized-filter-tag="wrapper"]');const r=[...t.querySelectorAll('[wized-filter-tag="tag-template"]')].filter((t)=>t.querySelector('[wized-filter-tag="tag-text"]').textContent.startsWith(`${e}:`));r.forEach((e)=>e.remove())};const m=(e,n)=>{const t=n.textContent.trim();const r=document.querySelector('[wized-filter-tag="wrapper"]');const i=[...r.querySelectorAll('[wized-filter-tag="tag-template"]')].find((e)=>e.querySelector('[wized-filter-tag="tag-text"]').textContent===`${e}: ${t}`);i&&i.remove()};const v=(e)=>{const t=e.target.closest('[wized-filter-tag="tag-remove"]');if(t){const n=t.closest('[wized-filter-tag="tag-template"]');const r=n.querySelector('[wized-filter-tag="tag-text"]').textContent.trim().split(":").map((e)=>e.trim());const i=r[0],o=r[1];c(i,o),m(n)}};document.addEventListener("change",r);document.addEventListener("click",v)});
+window.Wized = window.Wized || [];
+window.Wized.push((Wized) => {
+  // Function to create and append a new tag template
+  const createTagTemplate = (category, value) => {
+    const tagTemplate = document.createElement("div");
+    tagTemplate.setAttribute("wized-filter-tag", "tag-template");
+    tagTemplate.classList.add("filter_tag");
+
+    const tagText = document.createElement("div");
+    tagText.setAttribute("wized-filter-tag", "tag-text");
+    tagText.classList.add("filter_tag-text");
+    tagText.textContent = `${category}: ${value}`;
+
+    const removeIcon = document.createElement("div");
+    removeIcon.setAttribute("wized-filter-tag", "tag-remove");
+    removeIcon.classList.add("filter_tag-remove");
+
+    tagTemplate.appendChild(tagText);
+    tagTemplate.appendChild(removeIcon);
+
+    const tagsWrapper = document.querySelector('[wized-filter-tag="wrapper"]');
+    if (tagsWrapper) {
+      tagsWrapper.appendChild(tagTemplate);
+    }
+  };
+
+  // Function to handle changes on input elements (checkbox, radio) and select elements
+  const handleInputAndSelectChange = (event) => {
+    const element = event.target;
+    const categoryElement = element.closest("[wized-filter-tag-category]");
+    if (categoryElement) {
+      const category = categoryElement.getAttribute(
+        "wized-filter-tag-category"
+      );
+      if (element.type === "checkbox") {
+        handleCheckboxChange(element, category);
+      } else if (element.type === "radio") {
+        handleRadioChange(element, category);
+      } else if (element.tagName.toLowerCase() === "select") {
+        handleSelectChange(element, category);
+      }
+    }
+  };
+
+  const handleCheckboxChange = (checkbox, category) => {
+    const label = checkbox.nextElementSibling;
+    if (checkbox.checked) {
+      createTagTemplate(category, label.textContent.trim());
+    } else {
+      removeTag(category, label.textContent.trim());
+    }
+  };
+
+  const handleRadioChange = (radio, category) => {
+    removeCategoryTags(category);
+    if (radio.checked) {
+      createTagTemplate(category, radio.nextElementSibling.textContent.trim());
+    }
+  };
+
+  const handleSelectChange = (select, category) => {
+    removeCategoryTags(category);
+    const selectedOption = select.options[select.selectedIndex];
+    if (selectedOption && selectedOption.value) {
+      createTagTemplate(category, selectedOption.text.trim());
+    }
+  };
+
+  // Function to remove all tags of a specific category
+  const removeCategoryTags = (category) => {
+    const tagsWrapper = document.querySelector('[wized-filter-tag="wrapper"]');
+    const tagsToRemove = [
+      ...tagsWrapper.querySelectorAll('[wized-filter-tag="tag-template"]'),
+    ].filter((tag) =>
+      tag
+        .querySelector('[wized-filter-tag="tag-text"]')
+        .textContent.startsWith(`${category}:`)
+    );
+    tagsToRemove.forEach((tag) => tag.remove());
+  };
+
+  // Revised Function to reset the corresponding input element
+  const resetInputElement = (category, value) => {
+    const categoryElements = document.querySelectorAll(
+      `[wized-filter-tag-category="${category}"]`
+    );
+    categoryElements.forEach((categoryElement) => {
+      if (categoryElement.querySelector('input[type="checkbox"]')) {
+        const checkbox = Array.from(
+          categoryElement.querySelectorAll('input[type="checkbox"]')
+        ).find((cb) => cb.nextElementSibling.textContent.trim() === value);
+        if (checkbox) {
+          // Manually update the checkbox state
+          checkbox.checked = false; // Uncheck the checkbox
+          // Manually trigger a 'change' event
+          const event = new Event("change", {
+            bubbles: true,
+            cancelable: true,
+          });
+          checkbox.dispatchEvent(event); // Dispatch the event
+        }
+      } else if (categoryElement.querySelector('input[type="radio"]')) {
+        const radioGroup = categoryElement.querySelector(
+          'input[type="radio"]'
+        ).name;
+        resetRadioGroup(radioGroup, category);
+      } else if (categoryElement.tagName.toLowerCase() === "select") {
+        categoryElement.selectedIndex = 0;
+        categoryElement.dispatchEvent(new Event("change"));
+      }
+    });
+  };
+
+  // Revised Function to reset a radio group
+  const resetRadioGroup = (radioGroupName, category) => {
+    const radios = document.querySelectorAll(
+      `input[type="radio"][name="${radioGroupName}"]`
+    );
+    radios.forEach((radio) => {
+      if (radio.checked) {
+        radio.checked = false;
+
+        // Find and reset the custom radio button control
+        const customRadio = radio
+          .closest(`[wized-filter-tag-category="${category}"]`)
+          .querySelector(".w-radio-input");
+        if (
+          customRadio &&
+          customRadio.classList.contains("w--redirected-checked")
+        ) {
+          customRadio.classList.remove("w--redirected-checked");
+        }
+
+        // Update the Wized variable for this radio group
+        const variableName = radio.getAttribute("wized-variable");
+        if (variableName) {
+          Wized.data.v[variableName] = null;
+          if (window.executeWizedRequestDebounced) {
+            window.executeWizedRequestDebounced();
+          }
+        }
+      }
+    });
+  };
+
+  // Function to remove a specific tag
+  const removeTag = (category, value) => {
+    const tagText = `${category}: ${value}`;
+    const tagsWrapper = document.querySelector('[wized-filter-tag="wrapper"]');
+    const tagToRemove = [
+      ...tagsWrapper.querySelectorAll('[wized-filter-tag="tag-template"]'),
+    ].find(
+      (tag) =>
+        tag.querySelector('[wized-filter-tag="tag-text"]').textContent ===
+        tagText
+    );
+    if (tagToRemove) {
+      tagToRemove.remove();
+    }
+  };
+
+  // Function to handle click on tag-remove element
+  const handleTagRemoveClick = (event) => {
+    const removeIcon = event.target.closest('[wized-filter-tag="tag-remove"]');
+    if (removeIcon) {
+      const tagTemplate = removeIcon.closest(
+        '[wized-filter-tag="tag-template"]'
+      );
+      const tagText = tagTemplate
+        .querySelector('[wized-filter-tag="tag-text"]')
+        .textContent.trim();
+      const [category, value] = tagText.split(":").map((s) => s.trim());
+
+      // Reset the corresponding Wized variable
+      updateWizedVariableOnTagRemoval(category, value);
+
+      // Reset input element and remove the tag
+      resetInputElement(category, value);
+      tagTemplate.remove();
+
+      // Reset current page to 1 and call the centralized function to execute Wized request
+      resetCurrentPageToFirst();
+      window.executeWizedRequest();
+    }
+  };
+
+  // Function to reset current page variable to 1
+  const resetCurrentPageToFirst = () => {
+    const currentPageVariableName = document
+      .querySelector('[wized-filter-element="filters"]')
+      ?.getAttribute("wized-filter-current-page");
+    if (
+      currentPageVariableName &&
+      Wized.data.v.hasOwnProperty(currentPageVariableName)
+    ) {
+      Wized.data.v[currentPageVariableName] = 1;
+    }
+  };
+
+  // Function to update the Wized variable when a tag is removed
+  const updateWizedVariableOnTagRemoval = (category, value) => {
+    const variableElement = document.querySelector(
+      `[wized-filter-tag-category="${category}"][wized-variable]`
+    );
+    if (variableElement) {
+      const variableName = variableElement.getAttribute("wized-variable");
+      const currentValue = Wized.data.v[variableName];
+
+      // Update or clear the variable based on its type (array, string, etc.)
+      if (Array.isArray(currentValue)) {
+        Wized.data.v[variableName] = currentValue.filter(
+          (item) => item !== value
+        );
+      } else {
+        Wized.data.v[variableName] = null; // or any default value you prefer
+      }
+
+      // Trigger any necessary updates
+      if (window.executeWizedRequestDebounced) {
+        window.executeWizedRequestDebounced();
+      }
+    }
+  };
+
+  // Attach event listeners to input elements and select elements
+  document.addEventListener("change", handleInputAndSelectChange);
+  document.addEventListener("click", handleTagRemoveClick);
+});
